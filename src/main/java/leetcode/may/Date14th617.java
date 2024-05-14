d523fd82185261cd7b1f2a1e5b1159ebec674783
zjh <1113591960@qq.com>
2024-05-15 00:06:52 +0800

    2024/5/14 第617题 合并二叉树

---
+++ src/main/java/leetcode/may/Date14th617.java
@@ -0,0 +1,23 @@
+package leetcode.may;
+
+import leetcode.TreeNode;
+
+public class Date14th617 {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        return dfs(root1, root2);
+    }
+
+    private TreeNode dfs(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+
+        root1.val = root1.val + root2.val;
+        root1.left = dfs(root1.left, root2.left);
+        root1.right = dfs(root1.right, root2.right);
+        return root1;
+    }
+}
