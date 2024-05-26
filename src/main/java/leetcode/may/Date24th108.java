5894c54951fb636021ed042a437ba9052a4ae988
zjh <1113591960@qq.com>
2024-05-26 21:35:35 +0800

    2024/5/24 第108题 将有序数组转换为二叉搜索树

---
+++ src/main/java/leetcode/may/Date24th108.java
@@ -0,0 +1,20 @@
+package leetcode.may;
+
+import leetcode.TreeNode;
+
+public class Date24th108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode dfs(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums, left, mid - 1);
+        root.right = dfs(nums, mid + 1, right);
+        return root;
+    }
+}
