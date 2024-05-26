0a33ab1058d81a85e065ad758bb1ca99239e2370
zjh <1113591960@qq.com>
2024-05-26 21:38:16 +0800

    2024/5/24 第538题 把二叉搜索树转换为累加树

---
+++ src/main/java/leetcode/may/Date25th538.java
@@ -0,0 +1,17 @@
+package leetcode.may;
+
+import leetcode.TreeNode;
+
+public class Date25th538 {
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
