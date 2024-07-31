07fc4bfbaa558cd8c26d223312c94f181640449b
zjh <1113591960@qq.com>
2024-08-01 00:00:47 +0800

    2024/7/26 第300题 最长递增子序列 通过

---
+++ src/main/java/leetcode/july/Date27th300.java
@@ -0,0 +1,20 @@
+package leetcode.july;
+
+import java.util.Arrays;
+
+public class Date27th300 {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+        return Arrays.stream(dp).max().getAsInt();
+    }
+}
