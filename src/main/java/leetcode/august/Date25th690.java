0975987cfe9818abc8078a8a7a8f087f0ddd5a95
zjh <1113591960@qq.com>
2024-08-27 00:12:03 +0800

    2024/8/26 第690题 员工的重要性 通过

---
+++ src/main/java/leetcode/august/Date25th690.java
@@ -0,0 +1,37 @@
+package leetcode.august;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Date25th690 {
+    Map<Integer, Employee> collect;
+
+    int result = 0;
+
+    public int getImportance(List<Employee> employees, int id) {
+        collect = employees.stream().collect(Collectors.toMap(employee -> employee.id, employee -> employee));
+        dfs(id);
+        return result;
+    }
+
+    public void dfs(int id) {
+        result += collect.get(id).importance;
+        List<Integer> subordinates = collect.get(id).subordinates;
+
+        for (int i = 0; i < subordinates.size(); i++) {
+            dfs(subordinates.get(i));
+        }
+    }
+}
+
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+
+    public int getId() {
+        return id;
+    }
+}
