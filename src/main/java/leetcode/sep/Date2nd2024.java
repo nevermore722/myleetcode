85bf206ff9578da3ba46b4c5a200498f0fc15c78
zjh <1113591960@qq.com>
2024-09-03 00:04:31 +0800

    2024/9/2 第2024题 考试的最大困扰度 通过

---
+++ src/main/java/leetcode/sep/Date2nd2024.java
@@ -0,0 +1,17 @@
+package leetcode.sep;
+
+public class Date2nd2024 {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int ans = 0;
+        int[] cnt = new int[2];
+        char[] arr = answerKey.toCharArray();
+        for (int l = 0, r = 0; r < arr.length; r++) {
+            cnt[arr[r] >> 1 & 1]++;
+            while (cnt[0] > k && cnt[1] > k) {
+                cnt[arr[l++] >> 1 & 1]--;
+            }
+            ans = Math.max(ans, r - l + 1);
+        }
+        return ans;
+    }
+}
